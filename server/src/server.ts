af6d26489748337562ca3a6ddc67a36599364340
João Zanin <jwzanin@gmail.com>
2022-11-17 00:44:12 -0300

    finish web page

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { z } from 'zod'
-import { PrismaClient} from '@prisma/client'
-import ShortUniqueId from 'short-unique-id'
+import jwt from '@fastify/jwt'
 
+import { authRoutes } from './routes/auth'
+import { gameRoutes } from './routes/game'
+import { guessRoutes } from './routes/guess'
+import { poolRoutes } from './routes/pool'
+import { userRoutes } from './routes/user'
 
-const prisma = new PrismaClient({
-  log: ['query'],
-})
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -18,44 +18,17 @@ async function bootstrap() {
     origin: true,
   })
 
-  fastify.get('/pools/count', async () => {
-    const count = await prisma.pool.count()
-
-    return { count }
+  // em produção isso precisa ser uma variavel ambiente
+  await fastify.register(jwt, {
+    secret: 'nlwcopabyzanin',
   })
 
-  fastify.get('/users/count', async () => {
-    const count = await prisma.user.count()
-
-    return { count }
-  })
-
-  fastify.get('/guesses/count', async () => {
-    const count = await prisma.guess.count()
-
-    return { count }
-  })
-
-  fastify.post('/pools', async (request, response) => {
-    const createPoolBody = z.object({
-      title: z.string()
-    })
-
-    const { title } = createPoolBody.parse(request.body)
-
-    const generate = new ShortUniqueId({length: 6})
-    const code = String(generate()).toUpperCase()
-
-    await prisma.pool.create({
-      data:{
-        title,
-        code,
-      }
-    })
-
-    return response.status(201).send({ code })
-  })
-
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(poolRoutes)
+  await fastify.register(userRoutes)
+  
   await fastify.listen({
     port: 3333,
     /*host: '0.0.0.0'*/
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,41 +3,84 @@ import appPreviewImage from '../assets/app-nlw-copa-preview.png'
 import logoImg from '../assets/logo.svg'
 import usersAvatarImg from '../assets/users-avatar-example.png'
 import checkIconImg from '../assets/icon-check.svg'
+import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
-// interface HomeProps {
-//   count: number,
-// }
+interface HomeProps {
+  poolCount: number,
+  guessCount: number,
+  userCount: number,
+}
 
-export default function Home() {
+export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent){
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      })
+
+      const { code } = await response.data
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso o codigo foi copiado para area de transferencia!')
+      setPoolTitle('')
+    } catch (error) {
+      console.log(error)
+      alert('Falha ao criar ao bolão, tente novamente!')
+    }
+  }
 
   return (
-    <>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
      <main>
       <Image src={logoImg} alt="NLW Copa"/>
-      <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
-      <div>
+      <h1 className='mt-[60px] text-white text-5xl font-bold leading-tight'>
+        Crie seu próprio bolão da copa e compartilhe entre amigos!
+      </h1>
+      <div className='mt-10 flex gap-2 items-center'>
         <Image src={usersAvatarImg} alt="Images de pessoas"/>
-        <strong><span>+12.592</span> pessoas já estão usando </strong>
+        <strong className='text-gray-100 text-xl'>
+          <span className='text-ignite-500'>+{props.userCount}</span> pessoas já estão usando 
+        </strong>
       </div>
-      <form>
-        <input type="text" required placeholder='Qual nome do seu bolão?'/>
-        <button type='submit'>CRIAR MEU BOLÃO</button>
+      <form onSubmit={createPool} className='mt-10 flex gap-2'>
+        <input 
+          className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
+          onChange={event => setPoolTitle(event.target.value)}
+          value={poolTitle}
+          type="text" 
+          required 
+          placeholder='Qual nome do seu bolão?'
+        />
+        <button
+          className='bg-yellow-500 px-6 py-4 rounded text-sm font-bold text-gray-900 hover:bg-yellow-700'
+          type='submit' 
+          >
+          CRIAR MEU BOLÃO
+        </button>
       </form>
-      <p>
+      <p className='text-gray-300 mt-4 text-sm leading-relaxed'>
         Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
       </p>
-      <div>
-        <div>
+      <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between'>
+        <div className='flex items-center gap-6'>
           <Image src={checkIconImg} alt="Icone de Check"/>
-          <div>
-            <span>+2.034</span>
+          <div className='text-gray-100 flex flex-col'>
+            <span className='font-bold text-2xl'>+{props.poolCount}</span>
             <span>Bolões criados</span>
           </div>
         </div>
-        <div>
+
+        <div className='w-px h-14 bg-gray-600'/>
+
+        <div className='flex items-center gap-6'>
           <Image src={checkIconImg} alt="Icone de Check"/>
-          <div>
-            <span>+192.847</span>
+          <div className='text-gray-100 flex flex-col'>
+            <span className='font-bold text-2xl'>+{props.guessCount}</span>
             <span>Palpites enviados</span>
           </div>
         </div>
@@ -45,20 +88,25 @@ export default function Home() {
      </main>
 
      <Image src={appPreviewImage} alt="Dois Celulares Exibindo uma previda aplicação" quality={100}/>
-    </>
+    </div>
     
   )
 }
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch('http://localhost:3333/pools/count')
-//   const data = await response.json()
+export const getStaticProps = async () => {
+  const [poolCountResoponse, guessCountResponse, userCountResponse] = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count')
+  ])
 
-//   console.log(data)
+  return {
+    props: {
+      poolCount: poolCountResoponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
+    },
 
-//   return {
-//     props: {
-//       count: data.count,
-//     }
-//   }
-// }
+    revalidate: 1000,
+  }
+}
